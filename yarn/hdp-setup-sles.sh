0dbf6788aa1e8b7aa65fa3407b58e18f09061967
root <root@ip-172-31-3-210.us-west-2.compute.internal>
2014-08-14 19:32:45 +0000

    Ignore zypper GPG checks

--- yarn/hdp-setup-sles.sh
+++ yarn/hdp-setup-sles.sh
@@ -7,5 +7,7 @@
 # Configure NTP
 zypper --non-interactive install ntp
 /etc/init.d/ntp start
+# Ignore zypper gpg checks
+zypper --no-gpg-checks search ntp
 # Disable selinux
 echo 0 > /selinux/enforce
