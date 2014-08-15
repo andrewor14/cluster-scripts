378c9e5254dcfab14c3aba03cb24438433244485
root <root@ip-172-31-11-149.us-west-2.compute.internal>
2014-08-15 01:25:02 +0000

    Fix SUSE format script to automatically fix GPT

--- scripts/format-drive-sles.sh
+++ scripts/format-drive-sles.sh
@@ -4,10 +4,10 @@
 # The names of devices and root volume sizes may not apply
 
 umount /mnt
-parted -s /dev/hda rm 1
-parted -s /dev/hda mkpart logical 0GB 40GB
-parted -s /dev/hda print list
+parted -s /dev/hda mklabel gpt
+parted -s /dev/hda mkpart primary 0GB 40GB
 parted -s /dev/hda name 1 new-partition
+parted -s /dev/hda print list
 mkfs.ext3 /dev/hda1
 mount /dev/hda1 /mnt
 lsblk
