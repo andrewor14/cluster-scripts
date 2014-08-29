0f1948c3ac1033a833d4482e655dfa015d438365
andrewor14 <andrewor14@gmail.com>
2014-08-28 18:21:56 -0700

    Fix bug with mount drive script

--- scripts/mount-drive.sh
+++ scripts/mount-drive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$#" -le 2 ]]; then
+if [[ "$#" -lt 2 ]]; then
   echo "Usage: ./mount-drive.sh [drive name] [mount point]"
   echo "  e.g. ./mount-drive.sh /dev/xvdb /mnt"
   exit 1
