fa1c3e9631baf04e8e51f00e5e9abd5f368fd548
andrewor14 <andrewor14@gmail.com>
2014-11-20 03:08:46 +0000

    Make mount script super dangerous

--- scripts/mount-drive.sh
+++ scripts/mount-drive.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# WARNING: This script is dangerous because it reformats your drive!
+# Currently it is hard-coded to use ext3.
+
 if [[ "$#" -lt 2 ]]; then
   echo "Usage: ./mount-drive.sh [drive name] [mount point]"
   echo "  e.g. ./mount-drive.sh /dev/xvdb /mnt"
   exit 1
 fi
 
-mount "$1" "$2"
+mkfs.ext3 "$1"
+mount -t ext3 "$1" "$2"
 lsblk
 
